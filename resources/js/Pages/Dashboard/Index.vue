122dcf3b09b62f705189c18d113b04b1cebf78dd
Nenad Živanović <nen.zivanovic@gmail.com>
2019-04-10 15:03:51 +0200

    Switch place of columns on dashboard

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -8,18 +8,18 @@
       <table class="w-full whitespace-no-wrap">
         <thead>
           <tr class="text-left font-bold">
-            <th class="px-6 pt-6 pb-4">Question</th>
             <th class="px-6 pt-6 pb-4">Question Sent At</th>
+            <th class="px-6 pt-6 pb-4">Question</th>
             <th class="px-6 pt-6 pb-4">Reply</th>
             <th class="px-6 pt-6 pb-4">Replied At</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="entry in entries" class="hover:bg-gray-200 focus-within:bg-gray-200">
+            <td class="border-t px-6 py-4">{{ entry.question_sent_at | formatDatetime }}</td>
             <td class="border-t px-6 py-4">
               <inertia-link :href="route('questions.show', entry.question_id)" class="text-blue-500 hover:text-blue-600 underline">{{ entry.question_message }}</inertia-link>
             </td>
-            <td class="border-t px-6 py-4">{{ entry.question_sent_at | formatDatetime }}</td>
             <td class="border-t px-6 py-4" v-html="entry.reply" />
             <td class="border-t px-6 py-4">{{ entry.replied_at | formatDatetime }}</td>
             <td class="border-t w-px">
