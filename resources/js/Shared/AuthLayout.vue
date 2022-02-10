35d94aec0b5683914b388f7ca563bdd013ce5238
Nenad Živanović <nenad@zoozdev.com>
2022-02-10 15:16:22 +0100

    Upgrade to latest versions of npm packages

--- resources/js/Shared/AuthLayout.vue
+++ resources/js/Shared/AuthLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="p-6 bg-green-700 min-h-screen flex justify-center items-center">
+    <Head :title="title" />
     <div class="w-full max-w-sm">
       <div class="block mx-auto w-full max-w-xs text-white text-4xl text-center font-bold" v-if="$slots.heading">
         <slot name="heading" />
@@ -13,25 +14,15 @@
 </template>
 
 <script>
+import { Head } from '@inertiajs/inertia-vue'
+
 export default {
+  components: {
+    Head,
+  },
+
   props: {
     title: String,
-  },
-
-  watch: {
-    title(title) {
-      this.updatePageTitle(title)
-    },
-  },
-
-  mounted() {
-    this.updatePageTitle(this.title)
-  },
-
-  methods: {
-    updatePageTitle(title) {
-      document.title = title ? `${title} | ${this.$page.props.app.name}` : this.$page.props.app.name
-    },
   },
 }
 </script>
